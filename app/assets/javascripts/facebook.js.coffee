040426fce0e2eb7b6122dfafb31d4d8fdb9da3de
pnmichalakis <pnmichalakis@gmail.com>
2014-12-06 15:57:01 -0500

    facebook login nearly works, missing some parameters

--- app/assets/javascripts/facebook.js.coffee
+++ app/assets/javascripts/facebook.js.coffee
@@ -8,7 +8,7 @@ jQuery ->
 
 
 window.fbAsyncInit = ->
-  FB.init(appId: 'YOUR-APP-ID', cookie: true)
+  FB.init(appId: '753677691367934', cookie: true)
 
   $('#sign_in').click (e) ->
     e.preventDefault()
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
+require 'dotenv'
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 'YOUR-APP-ID-HERE', 'YOUR-APP-SECRET-HERE'
+  provider :facebook, ENV['APPID'], ENV['APPSECRET']
 end
